175b7a044391a7d0c7eccc684645d8ccfb2471b3
Signior-X <b19188@students.iitmandi.ac.in>
2020-08-10 14:50:51 +0530

    Updates

---
+++ precache-manifest.b6032fcaf84450c16d52d2f0e0567824.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4f620e6853959a5966bd8df0262545b",
+    "url": "/la-musica/index.html"
+  },
+  {
+    "revision": "c6bd438d26df5c574e92",
+    "url": "/la-musica/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "407cd2fc13e8498334a2",
+    "url": "/la-musica/static/js/2.548f6b9d.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/la-musica/static/js/2.548f6b9d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6bd438d26df5c574e92",
+    "url": "/la-musica/static/js/main.4344afdb.chunk.js"
+  },
+  {
+    "revision": "26b6684efb9f307955f0",
+    "url": "/la-musica/static/js/runtime-main.b225adb9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/la-musica/static/media/logo.5d5d9eef.svg"
+  }
+]);
